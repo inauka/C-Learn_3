a121292e8257cc72b388c61defdce7f3d5d315a2
Dmitry Korytniy <kor_dima@inbox.ru>
2021-03-04 13:22:20 +0300

    Добавьте файлы проекта.

---
+++ Learn_3.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+#include <stdint.h>
+#include <string>
+
+int main()
+{
+    std::string s = "Hello World";
+    std::cout << s << "\n";
+   
+    std::cout << s.length()<<"\n";
+    std::cout <<s.erase(1)<< "\n" << s[s.size() - 1] << "\n";
+}
+
