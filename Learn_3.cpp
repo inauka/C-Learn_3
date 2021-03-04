1b4446411a4919c388a169713525fb9ff208c74e
Dmitry Korytniy <kor_dima@inbox.ru>
2021-03-04 13:22:20 +0300

    апДобавьте файлы проекта.

---
+++ Learn_3.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+#include <stdint.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int b = 26; //параметр N для чет
+    for(int x=0; x<=b; x+=2)
+    {
+        cout << x << endl;
+    }
+    
+    cout <<  "\n\n\n" << endl;
+
+   /* нечетные
+   
+   int d = 27; // N для нечет
+    for(int z=1; z<=d; z+=2)
+    {
+        cout << z << endl;
+    }
+    
+    */
+}
+
